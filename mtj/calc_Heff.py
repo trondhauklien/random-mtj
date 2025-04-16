5ee30072854154c57466a73d73705e2dde26bb64
pietrosantini <pietro.santini02@gmail.com>
2025-04-16 18:56:37 +0200

    Update calc_Heff.py

--- mtj/calc_Heff.py
+++ mtj/calc_Heff.py
@@ -1,10 +1,15 @@
 import numpy as np
 import numpy.typing as ntp
+import scipy.constants as cst
 
 
 def calc_Heff(
     m: ntp.NDArray[np.float64],
-    # More parameters goes here
+    K_u: float,
+    M_s: float,
+    u_k: ntp.NDArray[np.float64],
+    H_app: ntp.NDArray[np.float64],
+    N: ntp.NDArray[np.float64]
 ) -> ntp.NDArray[np.float64]:
     """Calculates the effective field
 
@@ -12,13 +17,27 @@ def calc_Heff(
     ----------
         m: array_like
             Current magnetization as a Numpy array of floats with shape (3,).
+        K_u: float
+            First order crystal anisotropy constant (J/m^3)
+        M_s: float
+            Saturation magnetization module (A/m)
+        u_k: array_like
+            Direction of easy axis as a Numpy array of floats with shape (3,).
+        H_app: array_like
+            Externally applied magnetic field as a Numpy array of floats with shape (3,).
+        N: array_like
+            Demagnetization tensor as a Numpy array of floats with shape (3,3).
+
 
     Returns
     -------
         H_eff: array_like
             The effective field as a Numpy array of floats with shape (3,).
     """
+    # magneto-crystalline anisotropy term
+    H_mca = 2*K_u/(cst.mu_0*M_s)*np.dot(u_k, m)*u_k
 
-    # The implementation goes here
+    # demagnetization energy term
+    H_d = -M_s*(np.matmul(N, m))
 
-    return np.array([0, 0, 0])  # TODO Replace this after implementing function
+    return H_mca + H_app + H_d
