a295f5c953dbcb3b51815786668fa4a00c688bf2
pietrosantini <pietro.santini02@gmail.com>
2025-05-06 15:34:34 +0200

    Untested version
    - Modified a bit the structure of main.py and the input arguments for calc_Heff and compute_H_th functions
    - Possibility to recompute H_thermal and H_eff at intermediate step in Heun's method (can choose independently)
    - Needs to be tested and compare the results with/without recomputation (also check computation times)

--- mtj/calc_Heff.py
+++ mtj/calc_Heff.py
@@ -5,15 +5,7 @@ import scipy.constants as cst
 
 def calc_Heff(
     m: ntp.NDArray[np.float64],
-    K_u: float,
-    M_s: float,
-    u_k: ntp.NDArray[np.float64],
-    p: ntp.NDArray[np.float64],
-    a_para: float,
-    a_orth: float,
-    V: float,
-    H_app: ntp.NDArray[np.float64],
-    N: ntp.NDArray[np.float64],
+    params: dict,
     stt_enable: bool = True,
 ) -> ntp.NDArray[np.float64]:
     """Calculates the effective field
@@ -22,24 +14,25 @@ def calc_Heff(
     ----------
         m: array_like
             Current magnetization as a Numpy array of floats with shape (3,).
-        K_u: float
+        params: dictionary with items:
+            K_u: float
             First order crystal anisotropy constant (J/m^3).
-        M_s: float
-            Saturation magnetization module (A/m).
-        u_k: array_like
-            Direction of easy axis as a Numpy array of floats with shape (3,).
-        p: array_like
-            Spin polarization unit vector as a Numpy array of floats with shape (3,).
-        a_para: float
-            Parallel STT coefficient.
-        a_ortho: float
-            Orthogonal STT coefficient.
-        V: float
-            Applied voltage (V).
-        H_app: array_like
-            Externally applied magnetic field as a Numpy array of floats with shape (3,).
-        N: array_like
-            Demagnetization tensor as a Numpy array of floats with shape (3,3).
+            M_s: float
+                Saturation magnetization module (A/m).
+            u_k: array_like
+                Direction of easy axis as a Numpy array of floats with shape (3,).
+            p: array_like
+                Spin polarization unit vector as a Numpy array of floats with shape (3,).
+            a_para: float
+                Parallel STT coefficient.
+            a_ortho: float
+                Orthogonal STT coefficient.
+            V: float
+                Applied voltage (V).
+            H_app: array_like
+                Externally applied magnetic field as a Numpy array of floats with shape (3,).
+            N: array_like
+                Demagnetization tensor as a Numpy array of floats with shape (3,3).
         stt_enable: bool
             True if the STT contribution should be computed.
 
@@ -49,15 +42,15 @@ def calc_Heff(
             The effective field as a Numpy array of floats with shape (3,).
     """
     # magneto-crystalline anisotropy term
-    H_mca = 2*K_u/(cst.mu_0*M_s)*np.dot(u_k, m)*u_k
+    H_mca = 2*params["K_u"]/(cst.mu_0*params["M_s"])*np.dot(params["u_k"], m)*params["u_k"]
 
     # demagnetization energy term
-    H_d = -M_s*(np.matmul(N, m))
+    H_d = -params["M_s"]*(np.matmul(params["N"], m))
 
     # STT term
     if stt_enable:
-        H_STT = a_para*V*np.cross(m,p) - a_orth*p*(V**2)
+        H_STT = params["a_para"]*params["V"]*np.cross(m,params["p"]) - params["a_ortho"]*params["p"]*(params["V"]**2)
     else:
         H_STT = np.zeros(3, dtype=np.float64)
 
-    return H_mca + H_app + H_d + H_STT
+    return H_mca + params["H_app"] + H_d + H_STT
--- mtj/llg_heun.py
+++ mtj/llg_heun.py
@@ -2,6 +2,8 @@ import numpy as np
 import numpy.typing as ntp
 
 from mtj.constants import GYROMAGNETIC_RATIO, VACUUM_PERMEABILITY
+from mtj.calc_Heff import calc_Heff
+from mtj.calc_Hth import compute_thermal_field
 
 
 def LLG_rhs(m_vec, H_vec, gamma0, alpha):
@@ -16,29 +18,81 @@ def LLG_rhs(m_vec, H_vec, gamma0, alpha):
 
 def LLG_Heun(
     m_i: ntp.NDArray[np.float64],
-    H_eff: ntp.NDArray[np.float64],
-    H_th: ntp.NDArray[np.float64],
+    params: dict,
+    T: float,
+    Vol: float,
     dt: float,
     alpha: float,
     gamma0: float = VACUUM_PERMEABILITY * GYROMAGNETIC_RATIO,
+    recompute_H_th: bool=True,
+    recompute_H_eff: bool=True
 ) -> ntp.NDArray[np.float64]:
-    """Calculates the next time step magnetization
+    """Calculates the next time step magnetization using Heun's method, with the possibility to
+    recompute the total field H_tot at the intermediate step.
 
-    Returns
-    -------
-        m_next: array_like
-            The next time step magnetization as a NumPy array of floats with shape (3,).
+    Parameters
+    ----------
+        params: dictionary with items:
+            K_u: float
+            First order crystal anisotropy constant (J/m^3).
+            M_s: float
+                Saturation magnetization module (A/m).
+            u_k: array_like
+                Direction of easy axis as a Numpy array of floats with shape (3,).
+            p: array_like
+                Spin polarization unit vector as a Numpy array of floats with shape (3,).
+            a_para: float
+                Parallel STT coefficient.
+            a_ortho: float
+                Orthogonal STT coefficient.
+            V: float
+                Applied voltage (V).
+            H_app: array_like
+                Externally applied magnetic field as a Numpy array of floats with shape (3,).
+            N: array_like
+                Demagnetization tensor as a Numpy array of floats with shape (3,3).
+        T: float
+            Temperature of the system (K)
+        Vol: float
+            Volume of the magnetic sample (m^3)
+        dt: float
+            Time step interval (s)
+        alpha: float
+            Damping coefficient (unit-less)
+        gamma0: float
+            VACUUM_PERMEABILITY * GYROMAGNETIC_RATIO
+        recompute_H_th: bool
+            compute a new value for the thermal field for the intermediate step
+        recompute_H_eff: bool
+            recompute the effective field for the magnetization at the intermediate point
     """
 
-    H_tot = H_eff + H_th
-
-    # Heun's method
+    # First evaluation
+    H_th = compute_thermal_field(alpha, T, params["M_s"], Vol, dt)
+    H_eff = calc_Heff(m_i,
+                      params,
+                      True)
+    H_tot = H_eff + H_th  # Note: the externally applied field H_app is included in H_eff
     k1 = LLG_rhs(m_i, H_tot, gamma0, alpha)
     m_temp = m_i + dt * k1
-    m_temp /= np.linalg.norm(m_temp)  # keep magnetization normalized
+    m_temp /= np.linalg.norm(m_temp)  # Normalize after k1
 
-    k2 = LLG_rhs(m_temp, H_tot, gamma0, alpha)
+    # Second evaluation
+    if recompute_H_th:
+        # compute a new random thermal field for the intermediate step (which represents
+        # a point in the successive time instant)
+        H_th = compute_thermal_field(alpha, T, params["M_s"], Vol, dt)
+    if recompute_H_eff:
+        # recomputing the effective field with the intermediate magnetization m_temp
+        H_eff = calc_Heff(m_temp,
+                          params,
+                          True)
+    if recompute_H_th or recompute_H_eff:
+        H_tot = H_th + H_eff
+    k2 = LLG_rhs(m_temp, H_tot, gamma0, alpha)  # Recalculate k2 with updated m_temp and H_tot
+
+    # Heun step
     m_next = m_i + (dt / 2) * (k1 + k2)
-    m_next /= np.linalg.norm(m_next)  # renormalize
+    m_next /= np.linalg.norm(m_next)  # Renormalize
 
     return m_next
