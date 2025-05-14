fe7f993cffd08a34e5b5c9c61fc28dba0d56d9c2
pietrosantini <pietro.santini02@gmail.com>
2025-05-14 16:14:39 +0200

    Various fixes
    Fixed current files (main.py, usage_without_STT.ipynb) to work with new version of the modules (llg_heun.py and calc_Heff.py)

--- mtj/llg_heun.py
+++ mtj/llg_heun.py
@@ -24,6 +24,7 @@ def LLG_Heun(
     dt: float,
     alpha: float,
     gamma0: float = VACUUM_PERMEABILITY * GYROMAGNETIC_RATIO,
+    stt_enable: bool=True,
     recompute_H_th: bool=True,
     recompute_H_eff: bool=True
 ) -> ntp.NDArray[np.float64]:
@@ -61,6 +62,8 @@ def LLG_Heun(
             Damping coefficient (unit-less)
         gamma0: float
             VACUUM_PERMEABILITY * GYROMAGNETIC_RATIO
+        stt_enable: bool
+            enables STT effect
         recompute_H_th: bool
             compute a new value for the thermal field for the intermediate step
         recompute_H_eff: bool
@@ -68,17 +71,20 @@ def LLG_Heun(
     """
 
     # First evaluation
-    H_th = compute_thermal_field(alpha, T, params["M_s"], Vol, dt)
+    if T>0:
+        H_th = compute_thermal_field(alpha, T, params["M_s"], Vol, dt)
+    else:
+        H_th = 0
     H_eff = calc_Heff(m_i,
                       params,
-                      True)
+                      stt_enable)
     H_tot = H_eff + H_th  # Note: the externally applied field H_app is included in H_eff
     k1 = LLG_rhs(m_i, H_tot, gamma0, alpha)
     m_temp = m_i + dt * k1
     m_temp /= np.linalg.norm(m_temp)  # Normalize after k1
 
     # Second evaluation
-    if recompute_H_th:
+    if recompute_H_th and T>0:
         # compute a new random thermal field for the intermediate step (which represents
         # a point in the successive time instant)
         H_th = compute_thermal_field(alpha, T, params["M_s"], Vol, dt)
