6a79363ded9504bc93010da446e4d59e14c90eb4
pietrosantini <pietro.santini02@gmail.com>
2025-05-18 09:42:26 +0200

    E tot and stable state detection
    - added new notebook to demonstrate how to use
    - added calc_e function to calc_Heff module
    - added LLG_Heun_Heff function to return also Heff (used to compute stable state threshold)
    
    To-Do: improve graphs
    
    Note: cannot detect stable state if thermal fluctuations are on (or at least should use higher threshold)

--- mtj/calc_Heff.py
+++ mtj/calc_Heff.py
@@ -2,6 +2,7 @@ from typing import Unpack
 import numpy as np
 import numpy.typing as npt
 import scipy.constants as cst
+import mtj.constants as m_cst
 from mtj.types import MaterialProps
 
 
@@ -46,3 +47,41 @@ def calc_Heff(
         H_STT = np.zeros(3, dtype=np.float64)
 
     return H_mca + kwargs["H_app"] + H_d + H_STT
+
+def calc_e(
+    m: npt.NDArray[np.float64 | np.int_],
+    stt_enable: bool = True,
+    **kwargs: Unpack[MaterialProps],
+) -> npt.NDArray:
+    """Calculates the effective field
+
+    Parameters
+    ----------
+        m: array_like
+            Current magnetization as a Numpy array of floats with shape (3,).
+        stt_enable: bool
+            True if the STT contribution should be computed.
+        **kwargs: MaterialProps
+            Dictionary defining the properties of the material.
+    Returns
+    -------
+        e: float
+            Total energy normalized per unit volume.
+    """
+    e_app = -cst.mu_0 * kwargs["M_s"] * np.dot(kwargs["H_app"],m)
+    N_d = np.diagonal(kwargs["N"])
+    e_dem = 0.5*cst.mu_0*(kwargs["M_s"]**2)*(N_d[0]*(m[0]**2)+
+                                             N_d[1]*(m[1]**2)+
+                                             N_d[2]*(m[2]**2))
+    e_k = kwargs["K_u"]*(1-np.dot(m,kwargs["u_k"])**2)
+
+    e_stt = 0
+    # From the STT torque: the parallel component is always orthogonal to m,
+    # leading to a precessional motion and no associated potential energy. The
+    # energy contribution comes from the perpendicular term ("a_ortho"), which
+    # we define by analogy with an externally applied magnetic field.
+    if stt_enable:
+        e_stt = -cst.mu_0*cst.mu_0*m_cst.GYROMAGNETIC_RATIO*kwargs["a_ortho"]*\
+                np.dot(m,kwargs["p"])
+
+    return e_app + e_dem + e_k + e_stt
--- mtj/llg_heun.py
+++ mtj/llg_heun.py
@@ -88,3 +88,73 @@ def LLG_Heun(
     m_next /= np.linalg.norm(m_next)  # Renormalize
 
     return m_next
+
+def LLG_Heun_Heff(
+    m_i: ntp.NDArray[np.float64],
+    T: float,
+    Vol: float,
+    dt: float,
+    alpha: float,
+    gamma0: float = VACUUM_PERMEABILITY * GYROMAGNETIC_RATIO,
+    stt_enable: bool = True,
+    recompute_H_th: bool = True,
+    recompute_H_eff: bool = True,
+    **kwargs: Unpack[MaterialProps],
+) -> ntp.NDArray[np.float64]:
+    """Calculates the next time step magnetization using Heun's method, with the possibility to
+    recompute the total field H_tot at the intermediate step. Returns both m and the effective field for the previous step.
+
+    Parameters
+    ----------
+        T: float
+            Temperature of the system (K)
+        Vol: float
+            Volume of the magnetic sample (m^3)
+        dt: float
+            Time step interval (s)
+        alpha: float
+            Damping coefficient (unit-less)
+        gamma0: float
+            VACUUM_PERMEABILITY * GYROMAGNETIC_RATIO
+        stt_enable: bool
+            enables STT effect
+        recompute_H_th: bool
+            compute a new value for the thermal field for the intermediate step
+        recompute_H_eff: bool
+            recompute the effective field for the magnetization at the intermediate point
+        **kwargs: MaterialProps
+            Dictionary defining the properties of the material.
+    """
+
+    # First evaluation
+    if T > 0:
+        H_th = compute_thermal_field(alpha, T, kwargs["M_s"], Vol, dt)
+    else:
+        H_th = 0
+    H_eff = calc_Heff(m_i, stt_enable, **kwargs)
+    H_tot = (
+        H_eff + H_th
+    )  # Note: the externally applied field H_app is included in H_eff
+    k1 = LLG_rhs(m_i, H_tot, gamma0, alpha)
+    m_temp = m_i + dt * k1
+    m_temp /= np.linalg.norm(m_temp)  # Normalize after k1
+
+    # Second evaluation
+    if recompute_H_th and T > 0:
+        # compute a new random thermal field for the intermediate step (which represents
+        # a point in the successive time instant)
+        H_th = compute_thermal_field(alpha, T, kwargs["M_s"], Vol, dt)
+    if recompute_H_eff:
+        # recomputing the effective field with the intermediate magnetization m_temp
+        H_eff = calc_Heff(m_temp, True, **kwargs)
+    if recompute_H_th or recompute_H_eff:
+        H_tot = H_th + H_eff
+    k2 = LLG_rhs(
+        m_temp, H_tot, gamma0, alpha
+    )  # Recalculate k2 with updated m_temp and H_tot
+
+    # Heun step
+    m_next = m_i + (dt / 2) * (k1 + k2)
+    m_next /= np.linalg.norm(m_next)  # Renormalize
+
+    return m_next, H_eff
