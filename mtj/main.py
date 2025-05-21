dcc1ebf815592b493ab28a303b3007cb3b738a1e
Trond Hauklien <36609741+trondhauklien@users.noreply.github.com>
2025-05-21 21:32:51 +0200

    Add plots for varying radii

--- mtj/main.py
+++ mtj/main.py
@@ -3,30 +3,30 @@ import numpy as np
 
 
 def main() -> None:
-    analysis = PerpSTT(num_samples=200, volume=5e-9 ** 2 * 1e-9 * np.pi, volt_ampliude=0.75)
+    analysis = PerpSTT(num_samples=200, volume=15e-9 ** 2 * 1e-9 * np.pi, volt_ampliude=0.75)
     analysis.do_simulations()
     analysis.plot_trajectories(range(5))
-    analysis = PerpSTT(num_samples=200, volume=5e-9 ** 2 * 1e-9 * np.pi, volt_ampliude=0.75)
+    analysis = PerpSTT(num_samples=200, volume=15e-9 ** 2 * 1e-9 * np.pi, volt_ampliude=0.75)
     analysis.m0 = -analysis.m0
     analysis.do_simulations()
     analysis.plot_trajectories(range(5))
 
-    analysis = PerpSTT(num_samples=200, volume=50e-9 ** 2 * 1e-9 * np.pi, volt_ampliude=0.75)
-    analysis.do_simulations()
-    analysis.plot_trajectories(range(5))
-    analysis = PerpSTT(num_samples=200, volume=50e-9 ** 2 * 1e-9 * np.pi, volt_ampliude=0.75)
-    analysis.m0 = -analysis.m0
-    analysis.do_simulations()
-    analysis.plot_trajectories(range(5))
+    # analysis = PerpSTT(num_samples=200, volume=50e-9 ** 2 * 1e-9 * np.pi, volt_ampliude=0.75)
+    # analysis.do_simulations()
+    # analysis.plot_trajectories(range(5))
+    # analysis = PerpSTT(num_samples=200, volume=50e-9 ** 2 * 1e-9 * np.pi, volt_ampliude=0.75)
+    # analysis.m0 = -analysis.m0
+    # analysis.do_simulations()
+    # analysis.plot_trajectories(range(5))
 
 
-    analysis = PerpSTT(num_samples=200, volume=100e-9 ** 2 * 1e-9 * np.pi, volt_ampliude=0.75)
-    analysis.do_simulations()
-    analysis.plot_trajectories(range(5))
-    analysis = PerpSTT(num_samples=200, volume=100e-9 ** 2 * 1e-9 * np.pi, volt_ampliude=0.75)
-    analysis.m0 = -analysis.m0
-    analysis.do_simulations()
-    analysis.plot_trajectories(range(5))
+    # analysis = PerpSTT(num_samples=200, volume=100e-9 ** 2 * 1e-9 * np.pi, volt_ampliude=0.75)
+    # analysis.do_simulations()
+    # analysis.plot_trajectories(range(5))
+    # analysis = PerpSTT(num_samples=200, volume=100e-9 ** 2 * 1e-9 * np.pi, volt_ampliude=0.75)
+    # analysis.m0 = -analysis.m0
+    # analysis.do_simulations()
+    # analysis.plot_trajectories(range(5))
 
 
 
